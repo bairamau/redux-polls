d4dc60869d8e5255d4ca16a8a7f5b3fed11e2893
Zaur Bairamau <bairamau.z@gmail.com>
2019-01-14 03:47:49 +0300

    add poll asving

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import LoadingBar from 'react-redux-loading'
 
 import Dashboard from './Dashboard'
 import Leaderboard from './Leaderboard'
+import AddPoll from './AddPoll'
 
 class App extends Component {
   componentDidMount() {
@@ -18,7 +19,7 @@ class App extends Component {
         {
           this.props.loading === true
             ? null
-            : <Leaderboard />
+            : <AddPoll />
         }
       </div>
     )
