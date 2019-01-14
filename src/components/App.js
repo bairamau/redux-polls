21087288f3b939afe2840d490fbf6727ebe94246
Zaur Bairamau <bairamau.z@gmail.com>
2019-01-14 05:02:52 +0300

    add poll view and answers

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import LoadingBar from 'react-redux-loading'
 import Dashboard from './Dashboard'
 import Leaderboard from './Leaderboard'
 import AddPoll from './AddPoll'
-
+import Poll from './Poll'
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData())
@@ -19,7 +19,7 @@ class App extends Component {
         {
           this.props.loading === true
             ? null
-            : <AddPoll />
+            : <Poll match={{params: {id:'loxhs1bqm25b708cmbf3g'}}}/>
         }
       </div>
     )
--- src/reducers/polls.js
+++ src/reducers/polls.js
@@ -3,6 +3,10 @@ import {
   ADD_POLL,
 } from '../actions/polls'
 
+import {
+  ADD_ANSWER,
+} from '../actions/answers'
+
 export default function polls(state = {}, action) {
   switch (action.type) {
     case RECEIVE_POLLS:
@@ -14,6 +18,22 @@ export default function polls(state = {}, action) {
       return {
         ...state,
         [action.poll.id]: action.poll,
+      }
+    case ADD_ANSWER:
+      const {
+        answer,
+        id,
+        authedUser
+      } = action
+      const poll = state[id]
+      const votesKey = answer + 'Votes'
+
+      return {
+        ...state,
+        [action.id]: {
+          ...poll,
+          [votesKey]: poll[votesKey].concat([authedUser])
+        }
       }
     default:
       return state
